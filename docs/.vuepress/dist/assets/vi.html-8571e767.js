2a98f4e93c3278976a4d6dc4832bb2841b7d93ab
ayachinene0100 <ayachinene0100@163.com>
2023-03-21 20:50:43 +0800

    部署

--- docs/.vuepress/dist/assets/vi.html-8571e767.js
+++
@@ -1,5 +0,0 @@
-import{_ as e,W as a,X as n,a1 as i}from"./framework-e9da4443.js";const s={},d=i(`<h1 id="vi" tabindex="-1"><a class="header-anchor" href="#vi" aria-hidden="true">#</a> Vi</h1><h2 id="搜索与跳转" tabindex="-1"><a class="header-anchor" href="#搜索与跳转" aria-hidden="true">#</a> 搜索与跳转</h2><h3 id="显示行号" tabindex="-1"><a class="header-anchor" href="#显示行号" aria-hidden="true">#</a> 显示行号</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>:set number
-
-<span class="token comment"># 取消显示行号</span>
-:set nonumber
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),r=[d];function c(t,l){return a(),n("div",null,r)}const o=e(s,[["render",c],["__file","vi.html.vue"]]);export{o as default};
